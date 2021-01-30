d19ffba49d1fd5fb88b8789d89f0c94b46bd1fa0
TGiaimo <tgiaimo1@gmail.com>
2021-01-30 17:45:57 -0500

    added search, related video methods

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,32 +1,72 @@
 import os
+import requests
 
 from flask import Flask
+from string import Template
+HTML_TEMPLATE = Template("""
+  <iframe src="https://www.youtube.com/embed/${youtube_id}" frameborder="0" allowfullscreen></iframe>""")
 
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+def checkURL(url):
+  resp = requests.head(url)
+  if(resp.status_code == 200):
+    return True
+  else:
+    return False
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+api_key = 'AIzaSyCkz_MBLJbd7cNX5A0fvuw7C_IvUCC9PWQ'
+search_url = 'https://www.googleapis.com/youtube/v3/search'
+app = Flask(__name__)
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
 
-    return app
+def search(query):
+  request = {
+    'part': "snippet",
+    'maxResults': 1,
+    'safesearch': 'moderate',
+    'type': 'video',
+    'videoDuration': 'short',
+    'videoEmbeddable': 'true',
+    'order': 'date',
+    'q': query,
+    'key': api_key
+  }
+  response = requests.get(search_url, params=request)
+  print(response.text)
+
+
+def related(videoId):
+  request = {
+    'part': "snippet",
+    'maxResults': 1,
+    'safesearch': 'moderate',
+    'type': 'video',
+    'videoDuration': 'short',
+    'videoEmbeddable': 'true',
+    'order': 'date',
+    'relatedToVideoId': videoId,
+    'key': api_key
+  }
+  response = requests.get(search_url, params=request)
+  print(response.text)
+
+
+@app.route('/')
+def homepage():
+  vidhtml = HTML_TEMPLATE.substitute(youtube_id='N1jUp1KztJA')
+  return """<h1>Rabbithole</h1>""" + vidhtml
+
+
+@app.route('/videos/<id>')
+def videos(id):
+  youtube_url = 'https://youtube.com/watch?v={video_id}.'.format(video_id=id)
+  if True == checkURL(youtube_url):
+    return HTML_TEMPLATE.substitute(youtube_id=id)
+  else:
+    html = """<h1>Video does not exist. Click here to return!</h1>"""
+  return html, 404
+
+
+if __name__ == '__main__':
+  app.run(debug=True, use_reloader=True)
